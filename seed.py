7a912654c2c85476ca13319dcf3c886d0c5b9901
Primrose <primrose.njoki@student.moringaschool.com>
2025-07-28 12:09:26 +0300

    added imports

---
+++ seed.py
@@ -0,0 +1,6 @@
+from app import app
+from config import db
+from models import User, Item, Claim, Comment, Reward, Image
+from faker import Faker
+import random
+from datetime import datetime, timedelta
