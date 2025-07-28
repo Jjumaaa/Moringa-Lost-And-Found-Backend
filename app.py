b81606ac7119c909fc586fb7b218df696049e019
Pascal <pascalrintareseketeti@gmail.com>
2025-07-28 12:11:44 +0300

    app configuration

--- app.py
+++ app.py
@@ -18,3 +18,29 @@ CORS(app,
          "https://lost-now-found.vercel.app"
      ],
        supports_credentials=True)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///moringa.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'super-secret-key'
+app.secret_key = 'shhh-very-secret'
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
+
+os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
+
+db.init_app(app)
+migrate = Migrate(app, db)
+api = Api(app)
+jwt = JWTManager(app)
+
+def admin_only(fn):
+    @wraps(fn)
+    @jwt_required()
+    def wrapper(args, **kwargs):
+        user = User.query.get(get_jwt_identity())
+        if user.role != "admin":
+            return jsonify({"error": "Admin access only"}), 403
+        return fn(args, **kwargs)
+    return wrapper
+
+def log_admin_action(admin_id, action_desc):
+    admin = User.query.get(admin_id)
+    print(f"[ADMIN LOG] {datetime.utcnow()} - {admin.username} ({admin.id}) - {action_desc}")
